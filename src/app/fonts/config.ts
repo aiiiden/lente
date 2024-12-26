912944fcf2b3741c5e914a653b46b8021bce72f4
aiiiden <hjjam100@gmail.com>
2024-12-26 16:26:22 +0900

    초기 UI 리셋

--- src/app/fonts/config.ts
+++ src/app/fonts/config.ts
@@ -1,34 +1,16 @@
-import { Poppins } from 'next/font/google';
+import { IBM_Plex_Sans_KR } from 'next/font/google';
 
-export const fontSans = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
+export const fontSans = IBM_Plex_Sans_KR({
+  weight: [
+    '100', // Thin : font-thin
+    '200', // Extra Light : font-extralight
+    '300', // Light : font-light
+    '400', // Regular : font-normal
+    '500', // Medium : font-medium
+    '600', // Semi Bold : font-semibold
+    '700', // Bold : font-bold
+  ],
+  subsets: ['latin', 'latin-ext'],
+  preload: true,
+  display: 'block',
 });
-
-// import localFont from 'next/font/local';
-// export const fontSans = localFont({
-//   src: [
-//     {
-//       path: './LINESeedJP_OTF_EB.woff',
-//       weight: '800',
-//       style: 'extrabold',
-//     },
-//     {
-//       path: './LINESeedJP_OTF_Bd.woff',
-//       weight: '700',
-//       style: 'bold',
-//     },
-//     {
-//       path: './LINESeedJP_OTF_Rg.woff',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: './LINESeedJP_OTF_Th.woff',
-//       weight: '100',
-//       style: 'thin',
-//     },
-//   ],
-//   adjustFontFallback: 'Arial',
-//   preload: true,
-// });
